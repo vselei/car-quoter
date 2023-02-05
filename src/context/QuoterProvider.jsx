16b1e111b9f570fc05be56c193c7570488a41326
Victor Seleimend <vsennaselei@gmail.com>
2023-02-05 14:09:17 -0300

    Algoritmo de cotação

--- src/context/QuoterProvider.jsx
+++ src/context/QuoterProvider.jsx
@@ -1,6 +1,6 @@
 import { createContext, useState } from 'react';
 
-import { getYearDif } from '../helpers';
+import { getYearDif, calcModel, calcPlan, moneyFormatter } from '../helpers';
 
 const QuoterContext = createContext();
 
@@ -11,6 +11,7 @@ const QuoterProvider = ({ children }) => {
     plan: ''
   });
   const [error, setError] = useState('');
+  const [result, setResult] = useState(0);
 
   const handleDataChange = e => {
     setData({
@@ -26,17 +27,22 @@ const QuoterProvider = ({ children }) => {
     // Diferença de anos
     const yearDif = getYearDif(data.year);
 
-    // Aumento de 3% por ano
+    // Diferença de 3% por ano
     result -= (yearDif * 3 * result) / 100;
 
     // Europeu 30%
     // Americano 5%
     // Asiático 5%
+    result *= calcModel(data.model);
 
     // Básico 20%
     // Completo 50%
+    result *= calcPlan(data.plan);
 
-    console.log('cotando...');
+    // Formatar dinheiro
+    result = moneyFormatter(result);
+
+    setResult(result);
   };
 
   return (
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,33 @@
-export const getYearDif = (year) => {
+export const getYearDif = year => {
   return new Date().getFullYear() - year;
-}
+};
+
+export const calcModel = model => {
+  let inc;
+
+  switch (model) {
+    case '1':
+      inc = 1.3;
+      break;
+    case '2':
+      inc = 1.15;
+      break;
+    case '3':
+      inc = 1.05;
+      break;
+    default:
+      break;
+  }
+
+  return inc;
+};
+
+export const calcPlan = plan => {
+  return plan === '1' ? 1.2 : 1.5;
+};
+
+export const moneyFormatter = qty =>
+  qty.toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'USD'
+  });
