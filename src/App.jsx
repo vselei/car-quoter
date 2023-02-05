bd19ef17810da56b8e3e86d305a8db237645ff89
Victor Seleimend <vsennaselei@gmail.com>
2023-02-05 11:44:06 -0300

    Inicialização do projeto

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import CarQuoter from "./components/CarQuoter"
+
+function App() {
+
+  return (
+    <CarQuoter />
+  )
+}
+
+export default App
---
+++ src/components/CarQuoter.jsx
@@ -0,0 +1,5 @@
+const CarQuoter = () => {
+  return <h1>Hello World</h1>
+}
+
+export default CarQuoter;
