b2d0ee6f1497517baa0fcb12371ec2f55877917d
Victor Seleimend <vsennaselei@gmail.com>
2023-02-05 16:24:35 -0300

    Otimizando

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,14 +1,16 @@
+import { useCallback, useMemo, useRef } from 'react';
 import useQuoter from '../hooks/useQuoter';
 import { MODELS, PLANS } from '../constants';
 
 const Result = () => {
   const { result, data } = useQuoter();
   const { model, plan, year } = data;
-
-  const modelName = MODELS[model - 1].name;
-  const planName = PLANS[plan - 1].name;
+  const yearRef = useRef(year);
 
   if (result === 0) return null;
+
+  const modelName = useMemo(() => MODELS[model - 1].name, [result]);
+  const planName = useMemo(() => PLANS[plan - 1].name, [result]);
 
   return (
     <div className="bg-gray-100 text-center mt-5 p-5 shadow">
@@ -23,7 +25,7 @@ const Result = () => {
       </p>
       <p className="my-2">
         <span className="font-bold">Ano: </span>
-        {year}
+        {yearRef.current}
       </p>
       <p className="my-2 text-2xl">
         <span className="font-bold">Cotação Total: </span>
